6310dc7d29f20393a7260ebfcb2ee65f6f807d70
trishaDas13 <trisha.das1308@gmail.com>
2023-10-25 09:55:58 +0530

    date done

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let label = document.querySelectorAll('label');
 let form = document.querySelector('form');
 let done = document.querySelector('payment_done');
 
+
+
 button.addEventListener('click', function(e){
     e.preventDefault();
 
@@ -42,7 +44,7 @@ button.addEventListener('click', function(e){
 
     //! >>>>>> Promise part <<<<<<<
     let myPromise = new Promise((resolve, reject) => {
-        if(input[3].value <= 50000 && input[3].value >= 0){
+        if(input[3].value <= 20000 && input[3].value >= 0){
             setTimeout(() => {
                 resolve();
             }, (Math.floor(Math.random() * 5000) + 1000));
@@ -53,6 +55,7 @@ button.addEventListener('click', function(e){
             }, (Math.floor(Math.random() * 5000) + 1000));
         }
     });
+    //! >>>>>> Resolve part <<<<<<<
     myPromise.then(() => {
         document.getElementById('processing').style.display = 'none';
         document.getElementById('progress').style.display = 'none';
@@ -66,13 +69,13 @@ button.addEventListener('click', function(e){
             <svg width="80" height="80" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill="#3b82f6" d="m23 12l-2.44-2.78l.34-3.68l-3.61-.82l-1.89-3.18L12 3L8.6 1.54L6.71 4.72l-3.61.81l.34 3.68L1 12l2.44 2.78l-.34 3.69l3.61.82l1.89 3.18L12 21l3.4 1.46l1.89-3.18l3.61-.82l-.34-3.68L23 12m-13 5l-4-4l1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8Z"/>
             </svg>
-            <p class="rs">₹ 1.00</p>
-            <p class="name">Paid to Naren Das</p>
-            <p class="num">6291286732</p>
+            <p class="rs">₹ ${input[3].value}.00</p>
+            <p class="name">Paid to ${input[0].value}</p>
+            <p class="num">${input[1].value}</p>
         </div>
         <div class="down">
-            <p class="date">October 24, 2023 10:54 PM</p>
-            <p class="ID">UPI transaction ID : 3681686864826482</p>
+            <p class="date">${createDate()}</p>
+            <p class="ID">UPI transaction ID : ${Math.floor(Math.random() * 50000000000000)}</p>
         </div> 
     
         `;
@@ -91,17 +94,23 @@ button.addEventListener('click', function(e){
 
 
 
-
-
-
-
-
-
+    //! >>>>>> Reject part <<<<<<<
     myPromise.catch(() => {
         console.log("reject");
     });
     
 });
 
-
-
+//todo ------- To create the date -------
+function createDate(){
+    let dates = new Date();
+    let monthearr = ["January", "February", "March", "April", "May", "June", ",July", "August", "September", "October", "November", "December"];
+  
+    let month = monthearr[dates.getMonth()];
+    let date = dates.getDate();
+    let year = dates.getFullYear();
+    let hour = dates.getHours();
+    let minute = dates.getMinutes();
+    let finalDate = `${month} ${date}, ${year} ${hour}:${minute}`;
+    return finalDate;
+}
